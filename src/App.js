0bda81e4409408f84ed2c8564edd6a520927ea5d
Joe Rivera <dvs_joe@hotmail.com>
2023-09-17 13:44:20 -0400

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css'; // Import your CSS file here
+import './App.css'; // Import your global CSS styles here
 import Header from './components/Header';
-import AboutMe from './components/AboutMe';
-import Work from './components/Work';
-import ContactMe from './components/ContactMe';
+import AboutMe from './components/About'; // Make sure the import path is correct
+import Projects from './components/Projects'; // Import your Projects component
+import ContactMe from './components/ContactMe'; // Import your ContactMe component
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Header />
       <main>
         <AboutMe />
-        <Work />
-        <ContactMe />
+        <Projects /> {/* Use Projects component here */}
+        <ContactMe /> {/* Use ContactMe component here */}
       </main>
       <footer></footer>
     </div>
